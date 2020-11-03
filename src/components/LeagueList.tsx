e8548c698c0f0b333fe6deb7858ded30f3ceecb5
Riley Shenk <rjshenk@gmail.com>
2020-11-03 10:02:51 -0500

    Adding link and svg to lat long

--- src/components/LeagueList.tsx
+++ src/components/LeagueList.tsx
@@ -22,17 +22,34 @@ export default function LeagueList({ leagues, title, leagueIdToDistanceMapping }
         return (
           <div className="px-6 py-3" key={id}>
             <div className="flex justify-between">
-              <div className="font-semibold text-gray-800 capitalize">{name}</div>
-              <div className="text-sm text-gray-700">${Intl.NumberFormat().format(price)}</div>
+              <div>
+                <div className="font-semibold text-gray-800 capitalize">{name}</div>
+                <div className="flex pt-2 text-xs font-medium text-gray-500 hover:text-blue-500">
+                  <svg
+                    className={`block w-4 h-4 mr-1 fill-current`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 20S3 10.87 3 7a7 7 0 1114 0c0 3.87-7 13-7 13zm0-11a2 2 0 100-4 2 2 0 000 4z" />
+                  </svg>
+                  <a
+                    href={`https://www.google.com/maps/search/?q=${latitude},${longitude}&ll=${latitude},${longitude}&z=10`}
+                  >
+                    ({latitude.toFixed(5)}, {longitude.toFixed(5)})
+                  </a>
+                </div>
+              </div>
+              <div>
+                <div className="text-sm font-medium text-gray-700">
+                  ${Intl.NumberFormat().format(price)}
+                </div>
+                {leagueIdToDistanceMapping && (
+                  <div className="float-right pt-2 text-xs font-medium tracking-wider text-gray-500">
+                    {leagueIdToDistanceMapping[id].toFixed(2)}mi
+                  </div>
+                )}
+              </div>
             </div>
-            {leagueIdToDistanceMapping && (
-              <span className="pr-2 text-sm font-medium tracking-wider text-gray-700">
-                {leagueIdToDistanceMapping[id].toFixed(2)}mi
-              </span>
-            )}
-            <span className="text-xs text-gray-600">
-              ({latitude.toFixed(5)}, {longitude.toFixed(5)})
-            </span>
           </div>
         );
       })}
