eaed58eb11d9c00c4825ffa2c49fb53d61bfdca2
Riley Shenk <rjshenk@gmail.com>
2020-11-08 00:31:52 -0500

    Adding disabled state to button

--- src/atomic_ui/Button.tsx
+++ src/atomic_ui/Button.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Button({ onClick, type, disabled, children, ...rest }) {
-  const disabledStyle = disabled ? 'opacity-50 cursor-not-allowed' : '';
+  const [disabledButtonClicked, setDisabledButtonClicked] = useState(false);
+  const disabledStyle = disabled && disabledButtonClicked ? 'opacity-50 cursor-not-allowed' : '';
+
+  function clickHandler() {
+    if (disabled) {
+      setDisabledButtonClicked(true);
+    } else {
+      onClick();
+    }
+  }
 
   return (
-    <button
-      {...{ onClick, type, disabled, rest }}
-      className={`${disabledStyle} px-4 py-2 text-sm font-medium leading-5 text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500`}
-    >
-      {children}
-    </button>
+    <div>
+      <button
+        {...{ type, disabled, rest }}
+        disabled={disabledStyle !== ''}
+        className={`${disabledStyle} inline px-4 py-2 text-sm font-medium leading-5 text-white bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500`}
+        onClick={clickHandler}
+      >
+        {children}
+      </button>
+    </div>
   );
 }
